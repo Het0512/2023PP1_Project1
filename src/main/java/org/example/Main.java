447f22f5b16fad1308ba0aa92e73d37c016b592b
euntaek <euntaek@handong.ac.kr>
2023-09-01 11:17:16 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Student s1 = new Student("22200787", "홍길동");
+        System.out.println(s1.toString());
+        System.out.println("끝!");
+    }
+}
